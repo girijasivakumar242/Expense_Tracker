7f4b8f609a580de910f20434a518c468585be61b
girijasivakumar242 <girija2462006@gmail.com>
2025-09-02 12:13:34 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Expensecontainer from './Component/Expensecontainer.jsx'
-import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
+
 function App() {
   return (
     <Expensecontainer/>
